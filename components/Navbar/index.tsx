83347d192f774e4f9dad71445af3edd67edb37a1
Nour Al Salhi <nuraym9998@gmail.com>
2024-09-16 11:06:45 +0300

    add img and svg on folder public

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex justify-between">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;

bb35cbf45c93c1cc25f48a13a3527a6a877310a9
Nour Al Salhi <nuraym9998@gmail.com>
2024-09-17 11:19:21 +0300

    isolate data link in constant file and use map in useMemo

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,44 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useMemo } from "react";
+import { NAV_ITEMS } from "@/constant/navbar";
 
 const Navbar = () => {
+  const renderedNavItems = useMemo(
+    () =>
+      NAV_ITEMS.map((item, index) => (
+        <Link
+          key={index}
+          href={item.href}
+          className="text-[16px] text-slate-700 justify-center cursor-pointer pb-1.5 transition-all hover:font-bold"
+        >
+          {item.label}
+        </Link>
+      )),
+    []
+  );
+
   return (
     <nav className="max-w-[100rem] mx-auto flex justify-between items-center p-8 lg:px-20">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={100} height={29} />
       </Link>
-      <ul className="hidden h-full gap-12 lg:flex">
-        <Link
-          href="/"
-          className="text-[16px] text-slate-700 justify-center cursor-pointer pb-1.5 transition-all hover:font-bold"
-        >
-          Home
-        </Link>
-        <Link
-          href="/"
-          className="text-[16px] text-slate-700 justify-center cursor-pointer pb-1.5 transition-all hover:font-bold"
-        >
-          How Hilink Work?
-        </Link>
-        <Link
-          href="/"
-          className="text-[16px] text-slate-700 justify-center cursor-pointer pb-1.5 transition-all hover:font-bold"
-        >
-          Services
-        </Link>
-        <Link
-          href="/"
-          className="text-[16px] text-slate-700 justify-center cursor-pointer pb-1.5 transition-all hover:font-bold"
-        >
-          Pricing
-        </Link>
-        <Link
-          href="/"
-          className="text-[16px] text-slate-700 justify-center cursor-pointer pb-1.5 transition-all hover:font-bold"
-        >
-          Contact Us
-        </Link>
-      </ul>
+      <ul className="hidden h-full gap-12 lg:flex">{renderedNavItems}</ul>
       <Link
         href="/login"
         className="bg-gray-800 hover:bg-black text-white font-bold px-6 py-3 rounded-full gap-3 lg:flex hidden"
---
+++ constant/navbar.ts
@@ -0,0 +1,7 @@
+export const NAV_ITEMS = [
+    { href: "/", label: "Home" },
+    { href: "/", label: "How Hilink Work?" },
+    { href: "/", label: "Services" },
+    { href: "/", label: "Pricing" },
+    { href: "/", label: "Contact Us" },
+  ];
